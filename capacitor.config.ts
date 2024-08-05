c2bea5c63ea5516e4c3c648aff56888abe434d8a
Sid <sid.ghodke@gmail.com>
2024-08-05 10:16:17 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Magic8Ball',
+  webDir: 'dist'
+};
+
+export default config;
