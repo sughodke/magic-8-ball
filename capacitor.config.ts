32aaef1d5d6d894414ca7c7bad53d254d6238a8e
Sid Ghodke <sid.ghodke+github@gmail.com>
2024-08-06 08:43:33 -0500

    Retitle project

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.feltclinic.8ball',
   appName: 'Magic8Ball',
   webDir: 'dist'
 };
